b4b57a8940c5372a530535ba80ca576e8daec21d
HoagSon <54257831+HoagSon@users.noreply.github.com>
2020-10-06 19:38:55 +0700

    Add files via upload
    Canberra distance

---
+++ Canberra/Canberra.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+from scipy.spatial import distance 
+import random
+
+df = pd.read_csv("Point_Data.csv")
+
+lp = []
+for i in df.itertuples():
+    lp.append((round(i.x,3), round(i.y,3)))
+
+random = random.choices(lp, k = 200)
+dis = []
+for i in range(len(lp)):
+    x1, y1 = lp[i]
+    p = np.array([x1, y1])
+    row = []
+    for j in range(len(lp)):
+        x2, y2 = lp[j]
+        q = np.array([x2, y2])
+        dist = distance.canberra(p,q)
+        row.append(round(dist, 3))
+    dis.append(row)
+    
+dfs = pd.DataFrame(dis)
+dfs.to_csv('result_1000_canberra.csv', index = False)
